3f667eb62cadbad8995aefcb8d67cbd326acdc41
hsmtkk <hsmtkk@gmail.com>
2024-01-21 09:06:50 +0900

    use Next Link instead of a

--- alpha/app/page.tsx
+++ alpha/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <Image src="/logo.png" alt="logo" width={500} height={486} />
       <h1>Welome to this NextJs Course!</h1>
       <p>Let&apos;s get started!</p>
-      <p><a href="/about">About Us</a></p>
+      <p><Link href="/about">About Us</Link></p>
     </main>
   )
 }

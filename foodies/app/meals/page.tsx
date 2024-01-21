a57b052da395b4559a4933d0561cb2c189329e37
hsmtkk <hsmtkk@gmail.com>
2024-01-21 17:37:39 +0900

    install better-sqlite3 module

--- foodies/app/meals/page.tsx
+++ foodies/app/meals/page.tsx
@@ -1,7 +1,22 @@
+import classes from "@/app/meals/page.module.css"
+import MealsGrid from "@/components/meals/meals-grid"
+import Link from "next/link"
+
 export default function MealsPage() {
     return (
-        <div>
-            <p>Meals</p>
-        </div>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite recipe and cook it yourself. It is easy and fun.</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     )
 }

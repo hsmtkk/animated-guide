1033ec472b4cc7a0277667c699ebf4537ac0175c
hsmtkk <hsmtkk@gmail.com>
2024-01-21 09:02:42 +0900

    add alpha project

---
+++ alpha/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <img src="/logo.png" />
+      <h1>Welome to this NextJs Course!</h1>
+      <p>Let&apos;s get started!</p>
+      <p><a href="/about">About Us</a></p>
+    </main>
+  )
+}
--- app/example/page.tsx
+++
@@ -1,9 +0,0 @@
-const Example = () => {
-    return (
-        <div>
-            <p>Example</p>
-        </div>
-    )
-}
-
-export default Example

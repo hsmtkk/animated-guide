c107121dc43ccb4c0e29e75f66d9b6f547a5a1e2
hsmtkk <hsmtkk@gmail.com>
2024-01-23 16:29:01 +0900

    up

--- foodies/app/layout.tsx
+++ foodies/app/layout.tsx
@@ -1,12 +1,17 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'NextLevel Food',
   description: 'Delicious meals, shared by a food-loving community.',
 };
 
-export default function RootLayout({ children }) {
+interface Props {
+  children?: ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body>
